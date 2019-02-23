c557bc3ac5092cad65c5233a40a13489ac9252c2
Chris Underwood <cunderwoodmn@gmail.com>
2019-02-22 20:11:06 -0600

    add first 4 components an our basic routs

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Teams from './Teams';
+import Players from './Players';
+import Navbar from './Nav';
 class App extends Component {
   render() {
-    return <div>React Router Course</div>;
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/teams" component={Teams} />
+          <Route exact path="/players" component={Players} />
+        </div>
+      </Router>
+    );
   }
 }
 
---
+++ src/components/Players.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Players extends React.Component {
+  render() {
+    return <div className="container two-column">Players</div>;
+  }
+}
